7c5a79de502bb3b663ccaf7ff2f8164d28c34113
Chintu <bcsf18m017@pucit.edu.pk>
2021-10-06 09:39:27 +0500

    First Commit

---
+++ Lecture2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lecture2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(sum(2,"3"));
+
+        }
+        public static int sum(int a,int b)
+        {
+            return a + b;
+        }
+        public static int sum(double a, double b)
+        {
+            return Convert.ToInt32(a)+Convert.ToInt32(b);
+        }
+        public static int sum(float a, float b)
+        {
+            return Convert.ToInt32(a) + Convert.ToInt32(b);
+        }
+
+        public static int sum(int a,String b)
+        {
+            return a + (Convert.ToInt32(b));
+        }
+        public static int sum(String a, int b)
+        {
+            return Convert.ToInt32(a) + b;
+        }
+        public static int sum(String a, String b)
+        {
+            return (Convert.ToInt32(a)) + (Convert.ToInt32(b));
+        }
+
+    }
+}
